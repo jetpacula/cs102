6633cd1a0afd5a94b12bba74bc3b5cb09bc2bc02
Jetpacula <melnikov.jr@gmail.com>
2021-07-19 23:26:13 +0300

    vignere done

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,8 +9,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(plaintext)):
+        if keyword[i % len(keyword)].isupper():
+            shift = ord(keyword[(i % len(keyword))]) - 65
+        else:
+            shift = ord(keyword[(i % len(keyword))]) - 97
+        if plaintext[i].isupper():
+            ciphertext += chr(((ord(plaintext[i]) + shift - 65) % 26) + 65)
+        elif plaintext[i].islower():
+            ciphertext += chr(((ord(plaintext[i]) + shift - 97) % 26) + 97)
+        else:
+            ciphertext += plaintext[i]
+
     return ciphertext
 
 
@@ -26,5 +39,15 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        if keyword[i % len(keyword)].isupper():
+            shift = ord(keyword[(i % len(keyword))]) - 65
+        else:
+            shift = ord(keyword[(i % len(keyword))]) - 97
+        if ciphertext[i].isupper():
+            plaintext += chr(((ord(ciphertext[i]) - shift - 65) % 26) + 65)
+        elif ciphertext[i].islower():
+            plaintext += chr(((ord(ciphertext[i]) - shift - 97) % 26) + 97)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
