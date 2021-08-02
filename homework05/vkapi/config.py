086526740ff79cd07b90a9a66f1c2b5c9411bdc3
jetpacula <53007449+jetpacula@users.noreply.github.com>
2021-08-02 16:51:52 +0300

    functions completed

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "c377a22b7a7c0f1f9013374f13be3682cb5d50bbb3e3c3b0bbeb87430dc84ca87ec027f88162be3e59f5e",
     "version": "5.126",
 }
--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -3,7 +3,8 @@ import math
 import time
 import typing as tp
 
-from vkapi import config, session
+from vkapi import session
+from vkapi.config import VK_CONFIG
 from vkapi.exceptions import APIError
 
 QueryParams = tp.Optional[tp.Dict[str, tp.Union[str, int]]]
@@ -16,7 +17,10 @@ class FriendsResponse:
 
 
 def get_friends(
-    user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
+    user_id: tp.Optional[int],
+    count: int = 5000,
+    offset: int = 0,
+    fields: tp.Optional[tp.List[str]] = None,
 ) -> FriendsResponse:
     """
     Получить список идентификаторов друзей пользователя или расширенную информацию
@@ -28,7 +32,19 @@ def get_friends(
     :param fields: Список полей, которые нужно получить для каждого пользователя.
     :return: Список идентификаторов друзей пользователя или список пользователей.
     """
-    pass
+    params = {
+        "access_token": VK_CONFIG["access_token"],
+        "v": VK_CONFIG["version"],
+        "count": count,
+        "user_id": user_id if user_id is not None else "",
+        "fields": ",".join(fields) if fields is not None else "",
+        "offset": offset,
+    }
+    response = session.get("friends.get", params=params)
+    document = response.json()
+    if "error" in document or not response.ok:
+        raise APIError(document["error"]["error_msg"])
+    return FriendsResponse(**document["response"])
 
 
 class MutualFriends(tp.TypedDict):
@@ -57,4 +73,44 @@ def get_mutual(
     :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    if target_uids is None:
+        params = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "source_uid": source_uid if source_uid is not None else "",
+            "target_uid": target_uid,
+            "order": order,
+        }
+        response = session.get(f"friends.getMutual", params=params)
+        document = response.json()
+        if "error" in document or not response.ok:
+            raise APIError(document["error"]["error_msg"])
+        return document["response"]
+
+    responses = []
+    if progress is None:
+        progress = lambda x: x
+    for i in progress(range(((len(target_uids) + 99) // 100))):
+        params = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "target_uids": ",".join(map(str, target_uids)),
+            "order": order,
+            "count": count if count is not None else "",
+            "offset": offset + i * 100,
+        }
+        response = session.get(f"friends.getMutual", params=params)
+        document = response.json()
+        if "error" in document or not response.ok:
+            raise APIError(document["error"]["error_msg"])
+        for arg in document["response"]:
+            responses.append(
+                MutualFriends(
+                    id=arg["id"],
+                    common_friends=arg["common_friends"],
+                    common_count=arg["common_count"],
+                )
+            )
+        if i % 3 == 2:
+            time.sleep(1)
+    return responses
